bcf5a03d493067b8dbe6746f09278f8da652c9ac
Hamza Khan <hamzakhann66@gmail.com>
2024-01-01 13:06:21 +0500

    compile and deployed  simplestorage contract using hardhat

---
+++ hardhat-simple-storage/scripts/deploy.js
@@ -0,0 +1,35 @@
+// imports
+const { ethers, run, network } = require("hardhat")
+
+// async main
+async function main() {
+  const SimpleStorageFactory = await ethers.getContractFactory("SimpleStorage")
+  console.log("Deploying contract...")
+  const simpleStorage = await SimpleStorageFactory.deploy()
+  console.log(simpleStorage.target)
+}
+
+
+const verify = async (contractAddress, args) => {
+  console.log("Verifying contract...")
+  try {
+    await run("verify:verify", {
+      address: contractAddress,
+      constructorArguments: args,
+    })
+  } catch (e) {
+    if (e.message.toLowerCase().includes("already verified")) {
+      console.log("Already Verified!")
+    } else {
+      console.log(e)
+    }
+  }
+}
+
+// main
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error)
+    process.exit(1)
+  })
